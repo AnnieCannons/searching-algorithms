623769f816c4b82d1967b66a1659345be880f07f
Laura Hackney <laura@anniecannons.com>
2024-02-04 11:23:38 -0500

    initial commit

---
+++ exercises.js
@@ -0,0 +1,39 @@
+// Question 1
+// Given an array of numbers, implement a function to perform linear search and find the index of a specific target number. If the target is not present in the array, return -1.
+
+
+function linearSearch(arr, target) {
+    // Your code here
+}
+
+// Example usage:
+const numbers = [1, 3, 5, 7, 9, 11, 13];
+const targetNumber = 7;
+
+console.log(linearSearch(numbers, targetNumber));  // Should output the index 3
+
+
+
+
+
+
+// Question 2
+// Given a sorted array of numbers, implement a function to perform binary search and find the index of a specific target number. If the target is not present in the array, return -1.
+
+
+function binarySearch(arr, target) {
+    // Your code here
+}
+
+// Example usage:
+const sortedNumbers = [1, 3, 5, 7, 9, 11, 13];
+const targetNumber2 = 9;
+
+console.log(binarySearch(sortedNumbers, targetNumber2));  // Should output the index 4
+
+
+
+
+// Question 3
+// Consider a phone book with a large number of entries, each containing names and corresponding phone numbers. You want to find the phone number associated with a specific name. Write a search algorithm to efficiently find the correct data. 
+
